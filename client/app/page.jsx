41790d3e43c2ca675825b69c31c0fa485beefb50
Alexander Marini <svenskost6@gmail.com>
2024-09-14 02:57:21 +0200

    depth system and demo buttons

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -20,6 +20,23 @@ export default function Home() {
             <div className='w-15 bg-primary-500 h-6 m-4'></div>
             <div className='w-16 bg-primary-500 h-6 m-4'></div>
          </div>
+         <div className='px-8 py-4 rounded-3xl w-fit h-fit absolute left-1/2 top-1/2 flex flex-col bg-primary-400'>
+            <div className='flex'>
+               <button className='bg-primary-400 mx-4 text-200 text-neutral-100 w-10 h-8 raised-1 hover:raised-4 rounded-2xl hover:scale-110 duration-200 shadow-primary-300'>
+                  Raised Button
+               </button>
+               <button className='bg-primary-400 mx-4 text-200 text-neutral-100 w-10 h-8 lowered-1 hover:lowered-4 rounded-2xl hover:scale-90 duration-200 shadow-primary-300'>
+                  Lowered Button
+               </button>
+            </div>
+            <div className='flex justify-center mt-5'>
+               <button className='bg-primary-400 text-200 flex justify-center items-center text-neutral-100 w-fit h-fit py-1 px-2 raised-3 hover:translate-y-1 hover:raised-5 rounded-2xl hover:scale-110 duration-200 shadow-primary-300'>
+                  <div className='flex justify-center items-center bg-primary-400 text-200 text-neutral-100 w-10 h-8 lowered-1 rounded-2xl shadow-primary-300 active:lowered-4 active:scale-90 duration-100'>
+                     Cool Button
+                  </div>
+               </button>
+            </div>
+         </div>
       </>
    )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,56 +6,56 @@ const tailwindConfig = {
       colors: {
          neutral: {
             // 'bg-neutral-500'
-            100: 'hsl(220, 18, 98)',
-            200: 'hsl(220, 16, 95)',
-            300: 'hsl(220, 14, 92)',
-            400: 'hsl(220, 12, 84)',
-            500: 'hsl(220, 10, 71)',
-            600: 'hsl(220, 12, 56)',
-            700: 'hsl(220, 14, 32)',
-            800: 'hsl(220, 16, 23)',
-            900: 'hsl(220, 18, 15)',
+            100: 'hsl(220, 18%, 98%)',
+            200: 'hsl(220, 16%, 95%)',
+            300: 'hsl(220, 14%, 92%)',
+            400: 'hsl(220, 12%, 84%)',
+            500: 'hsl(220, 10%, 71%)',
+            600: 'hsl(220, 12%, 56%)',
+            700: 'hsl(220, 14%, 32%)',
+            800: 'hsl(220, 16%, 23%)',
+            900: 'hsl(220, 18%, 15%)',
          },
          primary: {
-            100: 'hsl(270, 100, 80)',
-            200: 'hsl(270, 90, 72)',
-            300: 'hsl(270, 80, 58)',
-            400: 'hsl(270, 60, 43)',
-            500: 'hsl(270, 55, 35)',
-            600: 'hsl(270, 60, 30)',
-            700: 'hsl(270, 80, 23)',
-            800: 'hsl(270, 90, 15)',
-            900: 'hsl(270, 100, 10)',
+            100: 'hsl(270, 100%, 80%)',
+            200: 'hsl(270, 90%, 72%)',
+            300: 'hsl(270, 80%, 58%)',
+            400: 'hsl(270, 60%, 43%)',
+            500: 'hsl(270, 55%, 35%)',
+            600: 'hsl(270, 60%, 30%)',
+            700: 'hsl(270, 80%, 23%)',
+            800: 'hsl(270, 90%, 15%)',
+            900: 'hsl(270, 100%, 10%)',
          },
          accent: {
             green: {
                // 'bg-accent-green-500'
-               100: 'hsl(135, 70, 70)',
-               300: 'hsl(135, 65, 56)',
-               500: 'hsl(135, 60, 39)',
-               700: 'hsl(135, 65, 34)',
-               900: 'hsl(135, 70, 28)',
+               100: 'hsl(135, 70%, 70%)',
+               300: 'hsl(135, 65%, 56%)',
+               500: 'hsl(135, 60%, 39%)',
+               700: 'hsl(135, 65%, 34%)',
+               900: 'hsl(135, 70%, 28%)',
             },
             red: {
-               100: 'hsl(355, 85, 40)',
-               300: 'hsl(355, 83, 35)',
-               500: 'hsl(355, 80, 30)',
-               700: 'hsl(355, 85, 25)',
-               900: 'hsl(355, 90, 20)',
+               100: 'hsl(355, 85%, 40%)',
+               300: 'hsl(355, 83%, 35%)',
+               500: 'hsl(355, 80%, 30%)',
+               700: 'hsl(355, 85%, 25%)',
+               900: 'hsl(355, 90%, 20%)',
             },
             yellow: {
-               100: 'hsl(50, 90, 70)',
-               300: 'hsl(50, 85, 60)',
-               500: 'hsl(50, 80, 50)',
-               700: 'hsl(48, 85, 45)',
-               900: 'hsl(48, 90, 40)',
+               100: 'hsl(50, 90%, 70%)',
+               300: 'hsl(50, 85%, 60%)',
+               500: 'hsl(50, 80%, 50%)',
+               700: 'hsl(48, 85%, 45%)',
+               900: 'hsl(48, 90%, 40%)',
             },
          },
       },
       fontSize: {
          // 'text-300'
-         100: '0.75rem',
-         200: '0.875rem',
+         100: '.75rem',
+         200: '.875rem',
          300: '1rem',
          400: '1.125rem',
          500: '1.25rem',
@@ -90,16 +90,17 @@ const tailwindConfig = {
       },
       letterSpacing: {
          // 'spacing-base'
-         tight: '-0.1em',
-         base: '0em',
-         wider: '0.1em',
-         widest: '0.4em',
+         tight: '-.1em',
+         base: '0',
+         wider: '.1em',
+         widest: '.4em',
       },
       spacing: {
-         // 'w/h/m/p/l/r/t/b-4'
-         1: '0.25rem',
-         2: '0.5rem',
-         3: '0.75rem',
+         // 'w/h/m/p-4'
+         0: 0,
+         1: '.25rem',
+         2: '.5rem',
+         3: '.75rem',
          4: '1rem',
          5: '1.5rem',
          6: '2rem',
@@ -116,37 +117,62 @@ const tailwindConfig = {
       },
       borderWidth: {
          // 'border-base'
-         none: '0rem',
-         thin: '0.0625rem',
-         base: '0.125rem',
-         thick: '0.25rem',
+         none: '0',
+         thin: '.0625rem',
+         base: '.125rem',
+         thick: '.25rem',
       },
-      // shadow system
+      depthEffect: {
+         raised: {
+            1: '0 -2px 0 var(--tw-shadow-color), 0 1px 1px hsla(0, 0%, 0%, .5)',
+            2: '0 -3px 0 var(--tw-shadow-color), 0 2px 2px hsla(0, 0%, 0%, .5)',
+            3: '0 -4px 0 var(--tw-shadow-color), 0 3px 3px hsla(0, 0%, 0%, .5)',
+            4: '0 -5px 0 var(--tw-shadow-color), 0 4px 4px hsla(0, 0%, 0%, .5)',
+            5: '0 -6px 0 var(--tw-shadow-color), 0 5px 5px hsla(0, 0%, 0%, .5)',
+         },
+         lowered: {
+            1: 'inset 0 -1px 0 var(--tw-shadow-color), inset 0 1px 1px hsla(0, 0%, 0%, .5)',
+            2: 'inset 0 -2px 0 var(--tw-shadow-color), inset 0 2px 2px hsla(0, 0%, 0%, .5)',
+            3: 'inset 0 -3px 0 var(--tw-shadow-color), inset 0 3px 3px hsla(0, 0%, 0%, .5)',
+            4: 'inset 0 -4px 0 var(--tw-shadow-color), inset 0 4px 4px hsla(0, 0%, 0%, .5)',
+            5: 'inset 0 -5px 0 var(--tw-shadow-color), inset 0 5px 5px hsla(0, 0%, 0%, .5)',
+         },
+      },
    },
    corePlugins: {
       lineHeight: false,
       fontWeight: false,
       letterSpacing: false,
+      boxShadow: false,
    },
    plugins: [
       function ({ addUtilities, e, theme, variants }) {
          const lineHeights = theme('lineHeight')
+         const fontWeights = theme('fontWeight')
+         const letterSpacings = theme('letterSpacing')
+         const depthEffects = theme('depthEffect')
+
          const lineUtilities = Object.keys(lineHeights).map(key => ({
             [`.line-${e(key)}`]: { lineHeight: lineHeights[key] },
          }))
-         addUtilities(lineUtilities, variants('lineHeight'))
-
-         const fontWeights = theme('fontWeight')
          const weightUtilities = Object.keys(fontWeights).map(key => ({
             [`.weight-${e(key)}`]: { fontWeight: fontWeights[key] },
          }))
-         addUtilities(weightUtilities, variants('fontWeight'))
-
-         const letterSpacings = theme('letterSpacing')
          const spacingUtilities = Object.keys(letterSpacings).map(key => ({
             [`.spacing-${e(key)}`]: { letterSpacing: letterSpacings[key] },
          }))
+         const raisedUtilities = Object.keys(depthEffects.raised).map(key => ({
+            [`.raised-${e(key)}`]: { boxShadow: depthEffects.raised[key] },
+         }))
+         const loweredUtilities = Object.keys(depthEffects.lowered).map(key => ({
+            [`.lowered-${e(key)}`]: { boxShadow: depthEffects.lowered[key] },
+         }))
+
+         addUtilities(lineUtilities, variants('lineHeight'))
+         addUtilities(weightUtilities, variants('fontWeight'))
          addUtilities(spacingUtilities, variants('letterSpacings'))
+         addUtilities(raisedUtilities, variants('raisedEffects'))
+         addUtilities(loweredUtilities, variants('loweredEffects'))
       },
    ],
 }
